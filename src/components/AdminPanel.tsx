49d65190f4330e6590e626b18bbddbf0428eee1c
Alperen Türkseven <20010310029@ogrenci.bartin.edu.tr>
2025-06-26 13:01:02 +0300

    adminPanel dosyası Eklendi

--- src/components/AdminPanel.tsx
+++ src/components/AdminPanel.tsx
@@ -33,7 +33,7 @@ export default function AdminPanel() {
           onClick={() => signOut({ callbackUrl: "/" })}
           className="mt-6 bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition"
         >
-          Çıkış Yap
+          Çıkış Yap.
         </button>
       </div>
     </div>
